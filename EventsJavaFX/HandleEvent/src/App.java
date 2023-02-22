bd69caabddffbeca4e57cf2c844769e6b2020f2b
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-02-22 06:36:58 +0300

    Event driven Programming JavaFX

--- EventsJavaFX/HandleEvent/src/App.java
+++ EventsJavaFX/HandleEvent/src/App.java
@@ -1,18 +1,50 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+  @Override
+  public void start(Stage primaryStage){
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
-    }
+      //Create a pane and set its properties
+      HBox pane = new HBox(10);
+      pane.setAlignment(Pos.CENTER);
+      Button btOk = new Button("OK");
+      OKHandlerClass handler1 = new OKHandlerClass();
+      btOk.setOnAction(handler1);
+      Button btCancel = new Button("Cancel");
+      CancelHandlerClass handler2 = new CancelHandlerClass();
+      btCancel.setOnAction(handler2);
+
+      pane.getChildren().addAll(btOk,btCancel);
+
+      Scene scene = new Scene(pane);
+      primaryStage.setTitle("EventHandler");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  class OKHandlerClass implements EventHandler<ActionEvent>{
+
+     @Override
+     public void handle(ActionEvent e){
+        System.out.println("OK button clicked");
+     }
+  }
+
+  class CancelHandlerClass implements EventHandler<ActionEvent>{
+      @Override
+      public void handle(ActionEvent e){
+         System.out.println("Cancel button clicked");
+      }
+  }
 }
